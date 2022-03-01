bc9c8a1850b140dc3a9dbe26e415445e9ffe474b
Macoyshev <64314280+macoyshev@users.noreply.github.com>
2022-03-01 22:21:39 +0300

    Update field.py

--- tictactoe/field.py
+++ tictactoe/field.py
@@ -11,18 +11,18 @@ class Field:
         self.COLUMNS_COUNT = Settings.colums
         self.WIDTH = Settings.getWidth()
         self.HEIGHT = Settings.getHeight()
-        self.emptyCellsCount = self.ROWS_COUNT * self.COLUMNS_COUNT
+        self.empty_cells_count = self.ROWS_COUNT * self.COLUMNS_COUNT
         self.body = [[None] * self.COLUMNS_COUNT for _ in range(self.ROWS_COUNT)]
 
-    def addUserMove(self, row: int, col: int) -> None:
+    def add_user_move(self, row: int, col: int) -> None:
         self.emptyCellsCount -= 1
         self.body[row][col] = "x"
 
-    def addBotMove(self, row: int, col: int) -> None:
+    def add_bot_move(self, row: int, col: int) -> None:
         self.emptyCellsCount -= 1
         self.body[row][col] = "o"
 
-    def isFull(self) -> bool:
+    def is_full(self) -> bool:
         return self.emptyCellsCount == 0
 
     def is_cell_free(self, row, col) -> bool:
